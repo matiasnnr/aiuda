7174f8620261bc3bcd6f9e01031c9ecf90c88e05
matiasnnr <matias.nnr@hotmail.com>
2021-10-23 16:46:06 -0300

    FIX: remove wrong code

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,24 +7,6 @@ import * as C from "@helpers/constants";
 import { useEffect } from "react";
 
 const Home: NextPage = () => {
-  useEffect(() => {
-    const gra = function (min, max) {
-      return Math.random() * (max - min) + min;
-    };
-
-    const gri = function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
-
-    const init = function () {
-      let items = document.querySelectorAll(".gallery li");
-      for (let i = 0; i < items.length; i++) {
-        items[i].style.minWidth = gra(30, 60) + "vw";
-      }
-    };
-    init();
-  }, []);
-
   return (
     <main>
       {/* Navbar */}
